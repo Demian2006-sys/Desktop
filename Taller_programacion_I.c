3ffdf483977e58cf94b70f7bdbf35e8ed83d5d14
Demian Durand <demian.durand@udla.edu.ec>
2025-03-27 18:26:45 -0500

    Descuentos y organizado

--- Taller_programacion_I.c
+++ Taller_programacion_I.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int stock, cantidad, opcion, valid = 0, result;
-    float precio, descuento, total_ganancias = 0, venta = 0;
+    int stock, cantidad, opcion, valid = 0, result, acum = 0;
+    float precio, descuento, total_ganancias = 0, venta = 0, vent_desc;
     char nombre[30], id[30], opc;
 
     do {
         printf("==========================================\n");
-        printf("Menu de opciones\n");
+        printf("___Menu de opciones___\n");
         printf("1. Registrar producto\n");
         printf("2. Vender producto\n");
         printf("3. Reabastecer producto\n");
@@ -17,7 +17,7 @@ int main() {
         printf("==========================================\n");
         printf("Seleccione una opcion: ");
         scanf("%d", &opcion);
-        getchar(); 
+        getchar();
         
         if (opcion >= 2 && opcion <= 5 && valid == 0) {
             printf("No se ha registrado ningún producto. Ingrese primero en la opcion 1.\n");
@@ -30,7 +30,6 @@ int main() {
             fflush (stdin);
             fgets(id, 30, stdin);
           
-
             printf("Ingrese el nombre del producto: ");
             fflush (stdin);
             fgets(nombre, 30, stdin);
@@ -51,10 +50,10 @@ int main() {
             do {
                 printf("Ingrese el precio unitario del producto: ");
                 result= scanf("%f", &precio);
-                if (result != 1) { 
+                if (result != 1) { // Si scanf no puede leer un número
                     printf("Entrada inválida. Por favor, ingrese un número entero.\n");
-                    while (getchar() != '\n'); 
-                    precio = -1; 
+                    while (getchar() != '\n'); // Limpia el buffer de entrada
+                    precio = -1; // Fuerza la repetición del bucle
                 } else if (precio <= 0) {
                     printf("Precio no valido, no se acepta el 0 o un número menor\n");
                 }
@@ -71,44 +70,37 @@ int main() {
             do {
                 printf("Ingrese la cantidad a vender: ");
                 result = scanf("%d", &cantidad);
-                if (result != 1) { 
+                if (result != 1) { // Si scanf no puede leer un número
                     printf("Entrada inválida. Por favor, ingrese un número entero.\n");
-                    while (getchar() != '\n'); 
-                    cantidad = -1; 
+                    while (getchar() != '\n'); // Limpia el buffer de entrada
+                    cantidad = -1; // Fuerza la repetición del bucle
                 }
                 else if (cantidad < 1) {
                     printf("La cantidad debe ser mayor a 0, vuelva a ingresar\n");
                 }
                 else if (cantidad > stock) {
                     printf("No existe la cantidad necesaria para esta venta\n");
+                    
                 }
+                
             } while (cantidad < 1 || cantidad > stock);
 
-            printf("Desea ingresar un descuento? (S/N): ");
-            scanf(" %c", &opc); 
-
             venta = cantidad * precio; 
+            acum = acum + cantidad;
 
-            if (opc == 'S' || opc == 's') {
-                do {
-                    printf("Ingrese el porcentaje de descuento: ");
-                    result = scanf("%f", &descuento);
-                    if (result != 1) {
-                        printf("Entrada inválida. Por favor, ingrese un número entero.\n");
-                        while (getchar() != '\n'); 
-                        descuento = -1; 
-                    }
-                    else if (descuento < 0 || descuento > 100) {
-                        printf("Descuento no válido, debe ser un valor entre 0 y 100.\n");
-                    }
-                } while (descuento < 0 || descuento > 100);
-            venta *= (1 - descuento / 100);
+            if (cantidad > 20) {
+                printf("Se aplicará un descuento del 10%% por comprar más de 20 productos.\n");
+                vent_desc = venta * 0.90;
+                printf("Precio final sin descuento; %.2f\n", venta);
+                printf("Precio final con descuento: %.2f\n", vent_desc);
+                total_ganancias += vent_desc;
+                stock -= cantidad;
+            }else{
+                
+                printf("El precio final de la venta es: %.2f\n", venta);
+                total_ganancias += venta;
+                stock -= cantidad;
             }
-
-            printf("El precio final de la venta es: %.2f\n", venta);
-            total_ganancias += venta;
-            stock -= cantidad;
-            
             break;
 
         case 3:
@@ -131,16 +123,21 @@ int main() {
 
         case 4:
             
-            printf("\nInformación del producto:\n");
-            printf("ID: %s\n", id);
-            printf("Nombre: %s\n", nombre);
-            printf("Stock disponible: %d\n", stock);
-            printf("Precio unitario: %.2f\n", precio);
+            printf("-------------------------------------------\n");
+            printf("| %-20s | %-10s |\n", "ID", id);
+            printf("-------------------------------------------\n");
+            printf("| %-20s | %-10s |\n", "Nombre", nombre);
+            printf("-------------------------------------------\n");
+            printf("| %-20s | %-10d |\n", "Stock disponible", stock);
+            printf("-------------------------------------------\n");
+            printf("| %-20s | $%-9.2f |\n", "Precio unitario", precio);
+            printf("-------------------------------------------\n");
             
             break;
 
-        case 5:
-         printf("Total de ganancias: $%.2f\n", total_ganancias);
+        case 5: 
+            printf("Productos vendidos: %d\n", acum);
+            printf("Total de ganancias: $%.2f\n", total_ganancias);
         
         break;
         }
